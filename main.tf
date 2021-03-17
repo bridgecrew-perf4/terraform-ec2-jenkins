aa17434575b1d788595fa80845eda2d57462290e
Mahesh <62394512+Maheshrjl@users.noreply.github.com>
2021-03-17 21:14:58 +0530

    added userdata instead of inline script

--- main.tf
+++ main.tf
@@ -118,31 +118,22 @@ resource "aws_instance" "ec2-server" {
   key_name                    = aws_key_pair.ssh-key.key_name
   count                       = 1
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update",
-      "sudo yum install openjdk-11-jdk -y",
-      "java -version",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo \\n",
-      "https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-      "sudo yum upgrade -y",
-      "sudo yum install jenkins java-1.8.0-openjdk-devel -y",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl status jenkins",
-      "sudo systemctl start jenkins",
-    ]
-  }
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    port        = 22
-    timeout     = "2m"
-    private_key = file("~/.ssh/id_rsa")
-  }
+  user_data = <<-EOF
+          #!/bin/bash
+          sudo yum -y update
+          sudo yum install wget -y
+          sudo wget -O /etc/yum.repos.d/jenkins.repo \
+          https://pkg.jenkins.io/redhat-stable/jenkins.repo
+          sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+          sudo yum upgrade -y
+          sudo yum install jenkins java-1.8.0-openjdk-devel -y
+          sudo systemctl daemon-reload
+          sudo systemctl start jenkins
+          sudo systemctl enable jenkins
+  EOF
+
   tags = {
-    "Name" = "${var.env_prefix}-${count.index}server"
+    "Name" = "${var.env_prefix}-ec2-instance"
   }
 }
 
